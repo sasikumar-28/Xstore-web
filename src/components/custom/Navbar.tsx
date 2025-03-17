2a919705df66afc7f95bb65f35b48ae8909de1c3
vaidik chouhan <vaidikchauhan333@gmail.com>
2025-03-17 16:23:39 +0530

    updated code for navbar

--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -9,16 +9,17 @@ import {
 import { Button } from "../ui/button";
 import { RouteParameter } from "@/type/route-type";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { cn } from "@/lib/utils";
-import { Menu, ChevronDown } from "lucide-react";
+import { Menu } from "lucide-react";
 import {
   Accordion,
   AccordionItem,
   AccordionTrigger,
   AccordionContent,
 } from "@/components/ui/accordion";
+import { useNavigate } from "react-router";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const route: RouteParameter[] = getRoute();
 
   return (
@@ -36,32 +37,81 @@ const Navbar = () => {
         </SheetHeader>
         <ScrollArea className="mt-4 h-[calc(100vh-100px)]">
           <Accordion type="multiple" className="space-y-4">
-            {route.map((item, index) => (
-              <AccordionItem key={index} value={`item-${index}`}>
-                <AccordionTrigger className="text-lg font-medium text-primary flex justify-between">
+            {route.map((item, index) =>
+              item?.children ? (
+                <AccordionItem key={index} value={`item-${index}`}>
+                  <AccordionTrigger className="text-lg font-medium text-primary flex justify-between">
+                    {item?.link ? (
+                      <div
+                        onClick={() => navigate(String(item?.link))}
+                        className="w-full text-left"
+                      >
+                        {item.name}
+                      </div>
+                    ) : (
+                      item.name
+                    )}
+                  </AccordionTrigger>
+                  <AccordionContent className="ml-4 space-y-2">
+                    {item.children?.map((child, index) =>
+                      child.children ? (
+                        <AccordionItem key={index} value={`child-${index}`}>
+                          <AccordionTrigger className="text-sm font-semibold text-muted-foreground flex justify-between">
+                            {child.link ? (
+                              <div
+                                onClick={() => navigate(String(child?.link))}
+                                className="w-full text-left"
+                              >
+                                {child.name}
+                              </div>
+                            ) : (
+                              child.name
+                            )}
+                          </AccordionTrigger>
+                          <AccordionContent className="ml-6 space-y-1">
+                            {child.children?.map((subChild, e) => (
+                              <div
+                                key={e}
+                                className="text-xs text-muted-foreground hover:text-primary transition"
+                              >
+                                {subChild.link ? (
+                                  <div
+                                    className="cursor-pointer hover:underline"
+                                    onClick={() =>
+                                      navigate(String(subChild?.link))
+                                    }
+                                  >
+                                    {subChild.name}
+                                  </div>
+                                ) : (
+                                  subChild.name
+                                )}
+                              </div>
+                            ))}
+                          </AccordionContent>
+                        </AccordionItem>
+                      ) : (
+                        <div
+                          key={index}
+                          className="cursor-pointer hover:underline"
+                          onClick={() => navigate(String(child?.link))}
+                        >
+                          {child.name}
+                        </div>
+                      )
+                    )}
+                  </AccordionContent>
+                </AccordionItem>
+              ) : (
+                <div
+                  key={index}
+                  className="cursor-pointer"
+                  onClick={() => navigate(String(item?.link))}
+                >
                   {item.name}
-                </AccordionTrigger>
-                <AccordionContent className="ml-4 space-y-2">
-                  {item.children?.map((child, index) => (
-                    <AccordionItem key={index} value={`child-${index}`}>
-                      <AccordionTrigger className="text-sm font-semibold text-muted-foreground flex justify-between">
-                        {child.name}
-                      </AccordionTrigger>
-                      <AccordionContent className="ml-6 space-y-1">
-                        {child.children?.map((subChild, e) => (
-                          <div
-                            key={e}
-                            className="text-xs text-muted-foreground hover:text-primary transition"
-                          >
-                            {subChild.name}
-                          </div>
-                        ))}
-                      </AccordionContent>
-                    </AccordionItem>
-                  ))}
-                </AccordionContent>
-              </AccordionItem>
-            ))}
+                </div>
+              )
+            )}
           </Accordion>
         </ScrollArea>
       </SheetContent>
