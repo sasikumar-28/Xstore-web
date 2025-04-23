612c346ffc7f5a1ed6746833506038ab05f2ff8e
vaidik chouhan <vaidikchauhan333@gmail.com>
2025-04-23 13:51:35 +0530

    removed arrow

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -51,22 +51,6 @@ const Header = () => {
           placeholder="Select Email"
         />
         <div className="flex items-center gap-4">
-          <div className="relative">
-            <div className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-              <svg
-                className="h-5 w-5 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-          </div>
           <div className="text-sm flex items-center gap-2">
             {images.map((image, index) => (
               <img
